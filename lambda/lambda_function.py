bd65bd57561b096d2870c40ac3ff288075f8965b
dcbrown <browncdonovan@gmail.com>
2025-11-21 10:40:45 -0500

    update

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,9 +1,7 @@
 import json
 
 def lambda_handler(event, context): 
-	return {
-		'statusCode': 200,
-		'body': json.dump(
-			'Hello from CICD Lambda'
-		)
-	}
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from CICD Lambda')
+    }
